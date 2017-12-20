2cda2efc057f5c9991dc5c99483b64a32ebb5e9f
Jeff Goodall <goodallhj@gmail.com>
2017-12-19 19:52:44 -0700

    added tests for suggest and count and insert

--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -13,23 +13,58 @@ describe('TRIE', () => {
   });
 
   describe('INSERT', () => {
+    it('should add "pizza" as letters into the trie', () => {
+      
+      trie.insert("pizza");
+      expect(trie.head.children.p.data).to.equal('p');
+      expect(trie.head.children.p.children.i.data).to.equal('i');
+    });
+      
     it('should add nodes as letters into the trie', () => {
 
       trie.insert('apple');
-      // console.log(JSON.stringify(trie, null, 4));
       expect(trie.head.children.a.data).to.equal('a');
       expect(trie.head.children.a.children.p.data).to.equal('p');
     });
 
-    //should check isWord is true
-    //should check length++
+    it('should check isWord is true', () => {
+      trie.insert('apple');
+      expect(trie.head.children.a.children
+        .p.children
+        .p.children
+        .l.children
+        .e.isWord).to.equal(true);
+    });
+    it('should check length++', () => {
+      expect(trie.length).to.equal(0);
+      trie.insert("pizza");
+      expect(trie.length).to.equal(1);
+      trie.insert('apple');
+      expect(trie.length).to.equal(2);
+    });
   });
 
   describe('COUNT', () => {
-    it('should return the word count that has been entered into the trie', () => {
-
+    it('should return the word count entered', () => {
+      trie.insert('apple');
+      trie.insert("pizza");
+      expect(trie.count()).to.equal(2);
     });
   });
+  
+  describe('SUGGEST', () => {
+    it('should return words that start with search string', () => {
+      trie.insert('apple');
+      trie.insert('pizza');
+      expect(trie.suggest('piz')).to.deep.equal([ 'pizza' ]);
+      expect(trie.suggest('ap')).to.deep.equal([ 'apple' ]);
+      trie.insert('pizzeria');
+      trie.insert('appitizer');
+      expect(trie.suggest('piz')).to.deep.equal([ 'pizza', 'pizzeria' ]);
+      expect(trie.suggest('ap')).to.deep.equal([ 'apple', 'appitizer' ]);
+    });
+  });
+ 
 
 
 
