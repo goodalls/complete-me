8f21cbf57688adcc64ad84c63d8809e9b8b77c05
Jeff Goodall <goodallhj@gmail.com>
2017-12-19 17:51:20 -0700

    Added suggest

--- tests/Node-test.js
+++ tests/Node-test.js
@@ -12,10 +12,6 @@ describe('NODE', () => {
     expect(node).to.exist;
   });
 
-  it('should default next to null', () => {
-    expect(node.next).to.equal(null);
-  });
-
   it('should take data and assign it to data prop', () => {
     expect(node.data).to.equal('pizza');
   });
--- tests/Trie-test.js
+++ tests/Trie-test.js
@@ -12,11 +12,42 @@ describe('TRIE', () => {
     expect(trie).to.exist;
   });
 
-  describe('insert', () => {
+  describe('INSERT', () => {
     it('should add nodes as letters into the trie', () => {
+
       trie.insert('apple');
-    
+      // console.log(JSON.stringify(trie, null, 4));
+      expect(trie.head.children.a.data).to.equal('a');
+      expect(trie.head.children.a.children.p.data).to.equal('p');
+    });
+
+    //should check isWord is true
+    //should check length++
+  });
+
+  describe('COUNT', () => {
+    it('should return the word count that has been entered into the trie', () => {
+
     });
   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
