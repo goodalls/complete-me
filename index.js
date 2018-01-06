33063d2fc12b4dd3a6abefd934b2a9c56dc075ae
Jeff Goodall <goodalls@live.com>
2018-01-06 15:19:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
-const Trie = require('../lib/Trie.js')
-const Node = require('../lib/Node.js')
+const Trie = require('./lib/Trie.js')
+const Node = require('./lib/Node.js')
 
 module.exports = {
   Trie,
